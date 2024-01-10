58699e047a632c127c857dc984eae73aa8373097
GorillaBearWolf <mrefsnider@icloud.com>
2024-01-10 04:38:31 -0500

    Add shebang // Update comments

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+# This file needs +x permissions.
+# In this terminal directory, run:
+# $ chmod +x install.sh
+# Then execute this script.
+
 chmod a+x rbu.sh
 cp rbu.plist ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/rbu.plist
